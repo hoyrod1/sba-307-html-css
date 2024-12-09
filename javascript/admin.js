405efcb5c98bd05967d64278fcaf04df7af4229e
Rodney St Cloud <hoyrod1@aol.com>
2024-12-08 19:26:10 -0500

    Completed the first draft of the admin.html page, the admin.css file and the admin.js file.

---
+++ javascript/admin.js
@@ -0,0 +1,7 @@
+console.log(`==================== Sanity Check For about.js ====================`);
+const clickMe = document.querySelector("#contact-div-button");
+// console.log(clickMe);
+clickMe.addEventListener("click", testButton);
+function testButton(e) {
+  alert("You have hit your Admin page target");
+}
